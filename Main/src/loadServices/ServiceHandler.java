5b4cdb8c522fc5662ab4b452924f6ae3129851f3
Pontus <Pontus.fredriksson1@gmail.com>
2019-02-12 15:57:41 +0100

    start on serviceHandler class and Service interface

---
+++ Main/src/loadServices/ServiceHandler.java
@@ -0,0 +1,13 @@
+package loadServices;
+
+import java.io.File;
+
+/**
+ * @author Pontus Fredriksson
+ */
+public class ServiceHandler {
+
+    public ServiceHandler(String fileLocation){
+
+    }
+}
