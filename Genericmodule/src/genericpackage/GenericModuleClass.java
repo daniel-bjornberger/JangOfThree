977265ee0c7e9fd20dcdeadd2154bd09c9bccdaa
Pontus <Pontus.fredriksson1@gmail.com>
2019-02-20 17:29:42 +0100

    Created a module that return links to all adress modules

--- Genericmodule/src/genericpackage/GenericModuleClass.java
+++ Genericmodule/src/genericpackage/GenericModuleClass.java
@@ -2,6 +2,7 @@ package genericpackage;
 
 import loadservices.Adress;
 import loadservices.Service;
+import loadservices.ServiceHandler;
 import loadservices.Type;
 import requestpackage.Request;
 import responsepackage.Response;
@@ -21,10 +22,34 @@ public class GenericModuleClass implements Service {
 
     @Override
     public void execute(Request request, Response response) {
+        ServiceHandler serviceHandler = new ServiceHandler("out/artifacts/lib/");
+        String currentClass;
+
         response.setStatus("200");
         response.setStatusMessage("OK");
         response.setContentType("text/plain");
-        response.setBody("GenericModule funkar");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Service service:serviceHandler.getServices()) {
+            if (service.getClass().getAnnotation(Type.class).value().equals("URL")) {
+                currentClass =(service.getClass().getAnnotation(Adress.class).value());
+                System.out.println("KLASSER SOM LADDATS IN:" +currentClass);
+
+                stringBuilder.append(
+                        "<p>Click <a href=\"http://localhost:8082"+currentClass+ "\">here</a> to go to "+currentClass+".</p>"
+                );
+            }
+        }
+        response.setBody(
+
+               // "<p>Click <a href=\"http://www.yahoo.com\">here</a> to go to yahoo.</p>"
+               // "<p>Click <a href=\"http://www.yahoo.com\">here</a> to go to yahoo.</p>"
+
+
+
+                stringBuilder.toString()
+
+        );
         response.setContentLength(response.getBody().length());
     }
 
