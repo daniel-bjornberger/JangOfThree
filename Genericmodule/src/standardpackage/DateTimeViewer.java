cdbc9e065932067ec2e1b6aecb6b0e4664bcaaae
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-02-21 18:38:47 +0100

    Add the class DateTimeViewer, and the html file 'datetimeinput'.

---
+++ Genericmodule/src/standardpackage/DateTimeViewer.java
@@ -0,0 +1,40 @@
+package standardpackage;
+
+
+import loadservices.Adress;
+import loadservices.Service;
+import loadservices.Type;
+import requestpackage.Request;
+import responsepackage.Response;
+import java.io.File;
+
+/**
+ * @author Daniel Björnberger
+ */
+@Adress("/date_time_viewer")
+@Type("URL")
+public class DateTimeViewer implements Service {
+
+
+    @Override
+    public void execute(Request request, Response response) {
+
+        System.out.println("Running Head Request...");
+        response.setStatus("200");
+        response.setStatusMessage("OK");
+        response.setContentType("text/html");
+        response.setBody("headajaxdemo.html");
+        response.setContentLength((int)(new File(new File("."),response.getBody()).length()));
+        response.setStaticFile(true);
+
+        System.out.println(response.toString());
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "HeadRequest";
+    }
+
+}
