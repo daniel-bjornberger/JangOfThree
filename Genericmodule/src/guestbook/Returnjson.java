06ea09fea4e6a0ade996fdd41406584142bfab77
joeliths <your>
2019-02-21 17:33:26 +0100

    Add ReturnJson Class

--- Genericmodule/src/guestbook/Returnjson.java
+++ Genericmodule/src/guestbook/Returnjson.java
@@ -12,35 +12,34 @@ import static com.sun.org.apache.xml.internal.serializer.utils.Utils.messages;
 import static java.nio.charset.StandardCharsets.ISO_8859_1;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-@Adress("/guestbook/update")
+@Adress("/json")
 @Type("URL")
-public class GuestBookUpdate implements Service {
-    public GuestBookUpdate() {
-    }
+
+public class Returnjson implements Service {
 
     @Override
     public void execute(Request request, Response response) {
 
-        System.out.println("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
-        System.out.println(request.getParsedData().size());
         String firstname=request.getParsedData().get("firstname");
         String messeges=request.getParsedData().get("messeges");
-        messeges=messeges.replace("+"," ");
 
-        GuestBookDB guestBookDB =new GuestBookDB();
-       guestBookDB.addVisitor(firstname,messeges);
 
-        System.out.println("Running Guestbook");
         response.setStatus("200");
         response.setStatusMessage("OK");
         response.setContentType("text/html");
-        response.setBody("guestbook.html");
-        response.setContentLength((int)(new File(new File("."),response.getBody()).length()));
-        response.setStaticFile(true);
+        response.setBody(String.valueOf("Hej"));
+        response.setContentLength(response.getBody().length());
+        response.setStaticFile(false);
     }
+
 
     @Override
     public String toString() {
-        return "GuestBookUpdate";
-    }
+        return "GenericModuleClass";
+        }
 }
+
+
+
+
+
